f954ae9fbf1d3d3f0b4d2b9817217fa7a89812e3
Aleksandr <aleksandr.rogachev1994@gmail.com>
2017-03-02 21:43:34 -0500

    Done.

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -1,3 +1,9 @@
 module SongsHelper
-
+  def artist_id_field(song)
+    if song.artist.nil?
+      collection_select :song, :artist_id, Artist.all, :id, :name
+    else
+      hidden_field_tag "song[:artist_id]", song.artist_id
+    end
+  end
 end
